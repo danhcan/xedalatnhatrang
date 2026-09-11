6533521ccecc54a407c35b60bc77c586ebc0731a
Danh <danhcan@gmail.com>
2026-09-12 00:24:45 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+// Service Worker – Thuê Xe Đà Lạt Nha Trang (PWA)
+// Chiến lược: network-first + cache dự phòng → nội dung luôn mới, mất mạng vẫn mở được trang
+const CACHE = 'xedalat-v1';
+const CORE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(CORE)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(e.request).then((m) => m || caches.match('./')))
+  );
+});
